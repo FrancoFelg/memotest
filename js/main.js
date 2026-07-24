77cec5f23a9c5ad81ca16ba097ee5d5900398519
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2026-07-24 20:17:31 -0300

    feat: Generación de ComponentLoader
    - Se ha creado un componente que permite cargar componentes genéricos, mejorando las buenas prácticas y reusabilidad de componentes.
    - Solo es posible si la aplicación es levantada en un servidor local, no funcionará si se abre el archivo HTML sin un servidor.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
+console.log("main")
 
 initLocalStorage();
+
+loadComponent('gencomp-header', basePath + 'header.html');
+loadComponent('gencomp-footer', basePath + 'footer.html');
 
 function logout(){
     deleteUserSession();
