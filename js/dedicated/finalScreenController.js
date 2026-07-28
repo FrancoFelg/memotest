abc913d3bafe8d5a048168f44bdb8769038e4f15
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2026-07-28 19:18:47 -0300

    fix: Campos añadidos en finalScreen.html

--- js/dedicated/finalScreenController.js
+++ js/dedicated/finalScreenController.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Intentar recuperar la partida recién finalizada del LocalStorage
     var lastGameStr = localStorage.getItem('last_finished_game');
     var lastGame = lastGameStr ? JSON.parse(lastGameStr) : null;
+    var db = getDatabase();
 
     // Guardia de seguridad: Si no hay registro de juego finalizado, lo mandamos al inicio
     if (lastGame === null) {
@@ -17,14 +18,32 @@ document.addEventListener('DOMContentLoaded', function() {
     var statAttempt = document.getElementById('statAttempt');
     var statMatches = document.getElementById('statMatches');
     var statFailures = document.getElementById('statFailures');
+    var statUsername = document.getElementById('statUsername');
+    var statDeckName = document.getElementById('statDeckName');
+    var statStreak = document.getElementById('statStreak');
     var gameResultTitle = document.getElementById('gameResultTitle');
     var timeObj = calculateTimeDifference(lastGame.startDatetime,lastGame.finalDatetime);
     
     // Inyectar los datos reales acumulados
+    console.log(lastGame)
     if (statTime) { statTime.textContent = timeObj.formattedTime; }
     if (statScore) { statScore.textContent = lastGame.score; }
     if (statAttempt) { statAttempt.textContent = lastGame.attempt; }
     if (statFailures) { statFailures.textContent = lastGame.failuresCount; }
+
+    if (statUsername) { statUsername.textContent = lastGame.playerName; }
+    if (statDeckName) {
+        var deckName = null;
+        for (var i = 0; i < db.decks.length; i++) {
+            if (db.decks[i].id === lastGame.deckUsed) {
+                deckName = db.decks[i].title;
+                break;
+            }
+        }
+        statDeckName.textContent = deckName;
+     }
+    if (statStreak) { statStreak.textContent = lastGame.actualStreak; }
+
     if (gameResultTitle) { 
         if(lastGame.isVictory == true) gameResultTitle.textContent = "¡Victoria!";
         else gameResultTitle.textContent = "Derrota";
